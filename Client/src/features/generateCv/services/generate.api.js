ce1112913d4fbb5acb5dba7a1f691b9ff288da64
vishalnaidu77 <vminorr@gmail.com>
2026-05-11 20:02:13 +0530

    Update API base URL to use production endpoint

--- Client/src/features/generateCv/services/generate.api.js
+++ Client/src/features/generateCv/services/generate.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://coverletter-ai-vjuw.onrender.com' || 'http://localhost:8000',
     withCredentials: true
 })
 
